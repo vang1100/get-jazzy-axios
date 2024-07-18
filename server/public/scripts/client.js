e4e85e36f228e72750692f761e7fe8b67990baa4
Padee Vang <vangpadee11@gmail.com>
2024-07-17 20:31:19 -0500

    working on base two

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -11,6 +11,7 @@ function onReady() {
             console.log(response);
             // quotesFromServer will be an Array of quotes
             let quotesFromServer = response.data;
+            console.log('quotes from server', quotesFromServer);
             let contentDiv = document.querySelector('#artistTableBody');
             for (let artist of quotesFromServer) {
                 contentDiv.innerHTML += `
@@ -20,14 +21,103 @@ function onReady() {
                     <td>${artist.died}</td>
                 </tr>
             `;
-            }
+            
+           
+        }
         }).catch(function (error) {
             // Code that will run on any errors from the server.
             console.log(error);
             alert('Something bad happened! Check the console for more details.')
-        });
+        })
+        
+        axios({
+            method: 'GET',
+            url: "/song"
+        })
+        .then(function(response) {
+            console.log(response);
+            let songsFromServer = response.data;
+            let songcontent = document.querySelector('#songTableBody');
+            for (let songs of songsFromServer) {
+                songcontent.innerHTML += `
+                 <tr>
+                    <td>${songs.title}</td>
+                    <td>${songs.artist}</td>      
+                </tr>
+                `;
+            }
+            
+        })
+        .catch(function (error){
+            console.log(error);
+        })
+
+       
+
+        };
 
     // TODO Add Axios request for /songs and display on DOM
-}
+   
+// axios({
+//     method: 'GET',
+//     URL: "/song"
+// })
+// .then(function(response) {
+//     console.log(response);
+//     let songsFromServer = response.data
+//     let songcontent = document.querySelector('#songTableBody');
+//     for (let songs of songsFromServer) {
+//         songcontent += ` 
+        
+//         `
+//     }
+// })
+
+// }
+
+
 
 onReady();
+
+
+// AXIOS POST HERE
+
+function submitHandler(event){
+    // stop the page from reloading
+    event.preventDefault();
+    console.log('running submit new quote');
+    //package up my quote into quote object
+
+    let name = document.querySelector('#name').value;
+    let birthDate = document.querySelector('#birth-Date').value;
+    let deathDate = document.querySelector('#death-Date').value;
+
+    console.log('inputs', name, birthDate, deathDate);
+
+    let artistToAdd = {
+        name: name,
+        born: birthDate,
+        died: deathDate
+    };
+
+    // axios post takes in 2 things
+        // 1) the path
+        // 2) the payload object
+
+    axios.post('/artist', artistToAdd)
+    .then(response => {
+        console.log('post/artist works', response);
+
+        document.querySelector('#name').value = '';
+        document.querySelector('#birth-Date').value = '';
+        document.querySelector('#death-Date').value = '';
+
+
+        onReady();
+    })
+    .catch (error=> {
+        console.log('post failed', error);
+        alert('something went wrong');
+    })
+
+}
--- server/server.js
+++ server/server.js
@@ -51,7 +51,63 @@ app.get('/artist', (req, res) => {
     res.send(artistListArray);
 });
 
+
+
+app.get('/song', (req, res) => {
+    res.send(songListArray);
+});
+
+
 // TODO - Add GET for songs
+
+
+
+// POST - To add new artist
+
+// app.post('/artists', (req,res) => {
+//     // The data (body) sent from the client is saved for us
+//     // in `req.body`
+//     // Note that without bodyParser setup, req.body will be undefined!
+//     console.log(`Get a POST request!`, req.body);
+
+//     // Grab the new quote from the request body
+//     let artist = req.body.artistToAdd;
+
+//     // Push the quote into our array
+//     console.log(`Adding new quote: `, artist)
+//     artistListArray.push(artist);
+
+//     // Send back a status code of 201
+//     res.sendStatus(201);
+// });
+
+app.post('/artist', function(req, res) {
+
+    console.log('in the POST /quotes route.')
+
+    // express adds a property to our request object called 'body'.
+    // That is where express will put the payload from our client.
+    // req.body has the payload sent to us by the client
+    // Note: This only works if your server knows how to work with JSON objects
+    // so we need to add in the boiler plate on line 8: app.use(express.json());
+    // Without that like, our POST routes won't know how to turn the body into an object.
+    console.log(req.body);
+    // For this to all work, I'm assuming with the code I'm about to write
+    // that req.body will have this structure:
+    // {
+    //     author: ____,
+    //     text: ____
+    // }
+    // I'm choosing that structure, because it matches the structure of all the other quotes on the server.
+
+    let newArtist = req.body;
+
+    // If it has the right structure, I can just add it to the list of quotes!
+    artistListArray.push(newArtist);
+
+    // And then tell the client I saved it.
+    res.sendStatus(201);
+});
 
 app.listen(PORT, () => {
     console.log('listening on port', PORT)
